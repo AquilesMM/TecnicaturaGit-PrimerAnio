c37306d197983e4f648248ea52d6abb1c575184a
AquilesMM <112222511+AquilesMM@users.noreply.github.com>
2022-10-30 04:45:08 -0300

    clase 11 python ejercicio

--- Python/Leccion7/Persona.py
+++ Python/Leccion7/Persona.py
@@ -1,4 +1,4 @@
-class Persona: #Esta clase hereda de la clase object, pero no se pone ya que es automatico
+class Persona:  # Esta clase hereda de la clase object, pero no se pone ya que es automatico
     def __init__(self, nombre, edad):
         self._nombre = nombre
         self._edad = edad
@@ -23,8 +23,8 @@ class Persona: #Esta clase hereda de la clase object, pero no se pone ya que es 
         print("Set edad")
         self._edad = edad
 
-    #SOBREESCRIBIMOS __str__ (DUNDER STR)
-    def __str__(self): #Este metodo viene de la clase object
+    # SOBREESCRIBIMOS __str__ (DUNDER STR)
+    def __str__(self):  # Este metodo viene de la clase object
         """
         significa override = sobreescribir
         este metodo sobreescribe los atributos, tod0 lo que recibe, y al ser miembro de la misma clase, puede acceder a los atributos (de esta clase o Herencia)
@@ -34,18 +34,17 @@ class Persona: #Esta clase hereda de la clase object, pero no se pone ya que es 
         return f'Persona: [nombre: {self._nombre}, edad: {self._edad} ]'
 
 
-
-class Empleado(Persona): #De esta menera le decimos a la clase empleado que es heredada de la clase Persona
+class Empleado(Persona):  # De esta menera le decimos a la clase empleado que es heredada de la clase Persona
     def __init__(self, nombre, edad, sueldo):
-        #utilizamos el contructor de la clase padre de todos, llamado: super
-        super().__init__(nombre, edad) #luego de super llamamos al metodo inicializador de la clase padre persona
-        #alli pasamos los parametros de la misma clase persona de quien llamamos el inicializador
-        #de esta manera hacemos uso de las caracteristicas de la clase persona
+        # utilizamos el contructor de la clase padre de todos, llamado: super
+        super().__init__(nombre, edad)  # luego de super llamamos al metodo inicializador de la clase padre persona
+        # alli pasamos los parametros de la misma clase persona de quien llamamos el inicializador
+        # de esta manera hacemos uso de las caracteristicas de la clase persona
 
-        #LOS METODOS INIT SE SOBREESCRIBEN EN LAS CLASES HIJAS
-
+        # LOS METODOS INIT SE SOBREESCRIBEN EN LAS CLASES HIJAS
 
         self._sueldo = sueldo
+
     """
     La clase padre e hija ademas de indicarle de quien hereda debemos hacer que se involucren para poder hacer
     uso de sus atributos, para ello la clase hija debe inicializar recibiendo los atributos de la clase padre
@@ -63,8 +62,8 @@ class Empleado(Persona): #De esta menera le decimos a la clase empleado que es h
         self._sueldo = sueldo
 
     def __str__(self):
-        return f'Empleado: [ Sueldo: {self._sueldo} ] {super().__str__()}' #aqui lo que hacemos es, luego de mostrar el str de empleado
-    #llamamos/sobre escribimos (segun profe) a el str de la clase persona, haciendo de esta manera que se muestren ambos __str__
+        return f'Empleado: [ Sueldo: {self._sueldo} ] {super().__str__()}'  # aqui lo que hacemos es, luego de mostrar el str de empleado
+    # llamamos/sobre escribimos (segun profe) a el str de la clase persona, haciendo de esta manera que se muestren ambos __str__
     # es como concatenar ambos str
 
 
@@ -76,7 +75,7 @@ Crear otro objeto, pasar los datos para nombre, edad y sueldo
 Mostrar estos datos, luego modificar y mostrar nuevamente
 """
 
-#ACOTACION DE ALGO APRENDIDO
+# ACOTACION DE ALGO APRENDIDO
 """
 A la hora de encapsular, puedo encapsular en la clase padre y no cambiara en nada la sintaxis de vinculacion 
 o uso de constructor super a la hora de heredar atributos, y su forma de uso no cambia, por lo que los metodos de 
--- Python/Leccion8/Vehiculo.py
+++ Python/Leccion8/Vehiculo.py
@@ -16,7 +16,73 @@ class Vehiculo:
     -Metodos(__init__(color, ruedas, tipo) y __str__()
 
     Crear un objeto de cada clase (padre, auto, bicicleta)
-
-
     '''
 
+    def __init__(self, color, ruedas):
+        self._color = color
+        self._ruedas = ruedas
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, color):
+        self._color = color
+
+    @property
+    def ruedas(self):
+        return self._ruedas
+
+    @ruedas.setter
+    def ruedas(self, ruedas):
+        self._ruedas = ruedas
+
+    def __str__(self):
+        return f'cantidad de ruedas: {self._ruedas}, color del vehiculo: {self._color}'
+
+
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self._velocidad = velocidad
+
+    @property
+    def velocidad(self):
+        return self._velocidad
+
+    @velocidad.setter
+    def velocidad(self, velocidad):
+        self._velocidad = velocidad
+
+    def __str__(self):
+        return f'{super().__str__()},Velocidad: {self._velocidad}'
+
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self._tipo = tipo
+
+    @property
+    def tipo(self):
+        return self._tipo
+
+    @tipo.setter
+    def tipo(self, tipo):
+        self._tipo = tipo
+
+    def __str__(self):
+        return f'{super().__str__()}, tipo: {self._tipo}'
+
+
+vehiculo1 = Vehiculo("Negro", 2)
+print(vehiculo1)
+print(f'El color del vehiculo1 es: {vehiculo1.color} y tiene: {vehiculo1.ruedas} ruedas')
+
+auto1 = Auto("Gris", 4, "200 km/hr")
+print(auto1)
+print(f'El color del auto es: {auto1.color}, tiene: {auto1.ruedas} ruedas y alcanza: {auto1.velocidad}')
+bicicleta1 = Bicicleta("Blanca", 2, "todo Terreno")
+print(bicicleta1)
+print(f'La bicicleta es {bicicleta1.color}, tiene {bicicleta1.ruedas} ruedas y es de tipo: {bicicleta1.tipo}')
