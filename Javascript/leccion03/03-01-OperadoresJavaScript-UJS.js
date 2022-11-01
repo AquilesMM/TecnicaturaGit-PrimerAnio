cd5476b5a768ba7c0aa8d64006f2f2512716ab23
AquilesMM <112222511+AquilesMM@users.noreply.github.com>
2022-10-31 22:34:16 -0300

    ejercicios clase 7

---
+++ Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,19 @@
+//Ejercicio para encontrar números pares
+
+let parImpar = 10;
+if (parImpar % 2 == 0){
+    console.log("El numero es PAR");
+}else{
+    console.log("El numero es IMPAR");
+}
+
+
+//Ejercicio es mayot de edad
+
+let persona = 19;
+if (persona >= 18){
+    console.log("La persona es mayor de edad")
+}
+else{
+    console.log("La persona es menor de edad")
+}
