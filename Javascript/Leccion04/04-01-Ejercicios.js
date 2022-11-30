61467b4d2b5e29100d7d2c03fc2cef3369ce2a1a
AquilesMM <112222511+AquilesMM@users.noreply.github.com>
2022-11-29 23:20:51 -0300

    Clase 11
    Función y ejercicio

--- Javascript/Leccion04/04-01-Ejercicios.js
+++ Javascript/Leccion04/04-01-Ejercicios.js
@@ -70,3 +70,107 @@ switch(mes){//no solo se pueden utilizar numeros, tambien cadenas
 
 }
 console.log("Bienvenido a la estacion de: "+estacion)
+
+
+//Evitar repetir tu codigo
+//Dry  don't repeat yourself
+
+
+
+let days = 'Sabado';
+switch (key) {
+    case 'Lunes':
+        console.log('Hoy es '+days);
+        break;
+    case 'Martes':
+        console.log('Hoy es '+days);
+        break;
+    case 'Miercoles':
+        console.log('Hoy es '+days);
+        break;
+    case 'Jueves':
+        console.log('Hoy es '+days);
+        break;
+    case 'Viernes':
+        console.log('Hoy es '+days);
+        break;
+    case 'Sabado':
+        console.log('Hoy es '+days);
+        break;
+    case 'Domingo':
+        console.log('Hoy es '+days);
+        break;
+    default:
+        console.log('Error en el ingreso del dia de la semana')
+        break;
+}
+
+//Version mejorada de el ejercicio anterior (ACORTA MUCHO CODIGOOOOO)
+/*
+aqui creamos un arreglo con los dias de la semana
+creamos una funcion donde necesitamos un parametro entero
+mostramos mensaje de error en caso de que no este en el parametro
+si esta en el parametro mostrara el dia de la semana, n-1 ya que los arreglos empiezan en 0
+luego llamamos a la funcion con console log y le pasamos el numero que queremos para indicar el dia entre parentesis
+*/
+let days2 = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+function getDay(n){
+    if(n<1 || n>7){
+    throw new Error('out of range');
+    }
+    return days2[n-1];
+}
+
+//Hacer un ejercicico similar al que esta echo, pero ahora con los meses del año, debes hacerlo con la estructura switch y luego
+//con la funcion en la pcion ejorada
+
+let moth = 11;
+switch (moth) {
+    case 1:
+        console.log('Es Enero');
+        break;
+    case 2:
+        console.log('Es Febrero');
+        break;
+    case 3:
+        console.log('Es Marzo');
+        break;
+    case 4: 
+        console.log('Es Abril');
+        break;
+    case 5:
+        console.log('Es Mayo');
+        break;
+    case 6:
+        console.log('Es Junio');
+        break;
+    case 7:
+        console.log('Es Julio');
+        break;
+    case 8:
+        console.log('Es agosto');
+        break;
+    case 9:
+        console.log('Es Septiembre');
+        break;
+    case 10:
+        console.log('Es Octurabre');
+        break;
+    case 11:
+        console.log('Es Noviembre');
+        break;
+    case 12:
+        console.log('Es diciembre');
+        break;
+    default:
+        console.log('Error con el mes introducido')
+        break;
+}
+let moth2 = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+function getmoth(n){
+    if(n<1 || n>12){
+    throw new Error('out of range');
+    }
+    return moth[n-1];
+}
+console.log(getmoth(1))
