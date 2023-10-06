f71f2abf4ad72ebf1f64e6b774667579e8b8fd71
AquilesIMM <aquilesmartinez490@gmail.com>
2023-10-06 01:36:33 -0300

    Clase 08

--- Java/Cuarto Semestre/Proyectos java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Cuarto Semestre/Proyectos java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,13 +1,32 @@
 package utn.tienda_libros;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroFrom;
 
+import java.awt.EventQueue;
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
 
+        ConfigurableApplicationContext contextoSpring =
+                new SpringApplicationBuilder(TiendaLibrosApplication.class)
+                        .headless(false)
+                        .web(WebApplicationType.NONE)
+                        .run(args); //Objeto de contexto Spring |configuracion de los headless
+        //.class es para que se pueda instanciar
+        //esta es otra forma de instanciar nuestra app de spring, obtenemos el contexto de spring indicando algunas caracteristicas mas
+        //sobre tod0 para que no se ejecute como una app web
+
+        //Ejecutamos el codigo para cargar el formulario
+        EventQueue.invokeLater(() -> {
+            //Obtenemos el objeto from a traves del spring
+            LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class); //instanciamos la clase a travez de la fabrica de spring
+            libroFrom.setVisible(true);
+        });
+    }
 }
--- Java/Cuarto Semestre/Proyectos java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/Cuarto Semestre/Proyectos java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -1,10 +1,17 @@
 package utn.tienda_libros.vista;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 
+@Component
+//agregando component hacemos que que el formulario de swin participe en la fabrica de spring
+//Entonces cuando mandemos a ejecutar la app a partir del contexto de spreen vamos a poder solicitar una instancia de este formulario
+//de esta forma empezamos a integrar swin con spring
 public class LibroFrom extends JFrame {
     //En el servicio inyectamos la dependencia de spring a travez de un atributo
     //aqui lo comenzaremos a inyectar a travez del constructor debido a que cuando se crea este objeto, necesitamos que ya este integrada
@@ -14,6 +21,9 @@ public class LibroFrom extends JFrame {
     //atributo
     LibroServicio libroServicio;
     private JPanel panel;
+    private JTable tablaLibros;
+    private DefaultTableModel tablaModeloLibros;
+
 
     @Autowired
     public LibroFrom(LibroServicio libroServicio){
@@ -26,6 +36,43 @@ public class LibroFrom extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//que se cierre la app con exito
         setVisible(true);//para visualizar el formulario
         setSize(900, 700);//dimencion de la ventana (alto ancho)
+        //Para obtener las dimenciones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        //utilizaremos la clase dimencion para obtener el tamaño de la pantalla y asi poder centrar la ventana para que se muestre con el objeto del toolkit
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        //definimos ejes
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.height - getHeight()/2);
+        setLocation(x, y);
     }
 
+    private void createUIComponents() {
+        this.tablaModeloLibros  = new DefaultTableModel(0, 5); //creamos un nuevo objeto y no especificamos ningun negristro, ningun renglon (seran dinamicos)
+        // y le indicamos 5 columnas
+        //creamos un array para las 5 columnas
+        String[] cabecera = {"Id", "Libro", "Autor", "Precio", "Existencias"}; //Cabecera
+        this.tablaModeloLibros.setColumnIdentifiers(cabecera);//el objeto tablaML con ayuda de este metodo le agregamos los identificadores de cada columna
+        //Instanciar el objeto de JTabl
+        this.tablaLibros = new JTable(tablaModeloLibros);
+        listarLibros();
+    }
+
+    private void listarLibros(){
+        //Limpiar la tabla
+        tablaModeloLibros.setRowCount(0);
+        //Obtener los libros de la BD
+        var libros = libroServicio.ListarLibros();
+        //Iteramos cada libro
+        libros.forEach((libro) -> { //Funcion Lambda
+            //Creamos cada registro para agregarlos a la tabla
+            Object [] renglonLibro = {
+              libro.getIdLibro(),
+              libro.getNombreLibro(),
+              libro.getAutor(),
+              libro.getPrecio(),
+              libro.getExistencias(),
+            };
+          this.tablaModeloLibros.addRow(renglonLibro);
+        });
+    }
 }
